e3f645060df3bf34211589feeed2fe453f6d4d78
Jose <gauranajose@gmail.com>
2021-03-07 18:32:47 +0800

    fourth commit

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+// Variables
+let currentAmount = 89914;
+let daysLeft = 56;
+let backers = 5007;
+const targetAmount = 100000;
+
+// Elements
+
 const bookmarkEl = document.getElementById('bookmark-container');
 const radioBtnEls = document.querySelectorAll('input[type=radio]');
 const backProjectBtnEl = document.getElementById('back-project-btn');
 const closeBtnMdlEl = document.getElementById('close-modal-btn');
 const selectRwdBtnEls = document.querySelectorAll('#get-started .btn');
+const modalOptionsEls = document.querySelectorAll('.modal .card');
 const pledgeAmountInEl = document.querySelectorAll('.accordion input');
-console.log(pledgeAmountInEl);
+const contBtnEls = document.querySelectorAll('.accordion .btn');
+const confBtnEl = document.querySelector('.conf .btn');
 
 // Event Handler Functions
 
@@ -44,6 +54,19 @@ const selectRewardBtnHandler = event => {
     togglePledge(event.currentTarget)
 }
 
+const confBtnClickHandler = event => {
+    document.getElementById('overlay').classList.remove('show');
+    event.currentTarget.parentElement.classList.remove('show');
+}
+
+const contBtnClickHanlder = event => {
+    document.querySelector('.conf').classList.add('show');
+    document.querySelector('.modal').classList.remove('show');
+    const element = event.currentTarget.parentElement;
+    const amount = element.querySelector('input[type=number]').value
+    updateUI(amount);
+}
+
 // Helper Functions
 
 const togglePledge = element => {
@@ -62,6 +85,24 @@ const getIndexOf = element => {
     return i;
 }
 
+const updateUI = amount => {
+    const current = document.getElementById('current');
+    const target = document.getElementById('target');
+    const backs = document.getElementById('backers');
+    const days = document.getElementById('days-left');
+    const progress = document.getElementById('progress-status');
+    if(amount) {
+        currentAmount += +amount;
+        backers++;
+    }
+    current.innerText = `\$${currentAmount.toLocaleString()}`;
+    target.innerText = `of \$${targetAmount.toLocaleString()} backed`;
+    backs.innerText = backers.toLocaleString();
+    days.innerText = daysLeft.toLocaleString();
+    const percent = currentAmount/targetAmount * 100;
+    progress.style.width = `${percent}%`;
+}
+
 // Add Event Listeners
 
 for (const el of radioBtnEls) {
@@ -72,6 +113,20 @@ for (const el of selectRwdBtnEls) {
     el.addEventListener('click', selectRewardBtnHandler);
 }
 
+for (const el of contBtnEls) {
+    el.addEventListener('click', contBtnClickHanlder);
+}
+
+for (const el of modalOptionsEls) {
+    el.addEventListener('click', event => {
+        event.currentTarget.querySelector('input[type=radio]').click();
+    })
+}
+
 bookmarkEl.addEventListener('click', bookmarkClickHandler);
 backProjectBtnEl.addEventListener('click', backProjectClickHandler);
 closeBtnMdlEl.addEventListener('click', closeBtnMdlHandler);
+confBtnEl.addEventListener('click', confBtnClickHandler);
+
+// On Init
+updateUI();
